5a3696878c3451a59165887d0d145cba256bf29c
guacs <guacs.guacs@gmail.com>
2024-03-02 15:22:58 +0530

    feat: allow overriding of dependencies

--- bitespeed/app.py
+++ bitespeed/app.py
@@ -4,10 +4,12 @@ from litestar import Litestar
 from litestar.types.composite_types import Dependencies
 
 
-def create_app() -> Litestar:
+def create_app(override_dependencies: Dependencies | None = None) -> Litestar:
     from bitespeed.db import get_connection
     from bitespeed.routes import identify
 
     dependencies: Dependencies = {"db_conn": get_connection}
+    if override_dependencies:
+        dependencies.update(override_dependencies)
 
     return Litestar([identify], dependencies=dependencies, debug=True)
